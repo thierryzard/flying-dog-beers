d319fbe8c9b879ef0af994826cb3b7411c93ae09
thierryzard <62876206+thierryzard@users.noreply.github.com>
2020-04-21 13:06:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -51,6 +51,26 @@ fig = go.Figure(data=[
     go.Bar(name='Vrouw', x=vrouw['Type'], y=vrouw['Aantal'], marker_color='#E3127B')
 ])
 
+fig.add_annotation(
+    dict(
+            x=0.99,
+            y=1,
+            showarrow=False,
+            text="geüpdatet op  {:%d/%m/%Y}".format(laatste_update),
+            xref="paper",
+            yref="paper",
+        font=dict(
+            size=10,
+            color="#FFFFFF"
+            ),
+        align="center",  
+        bordercolor="#c7c7c7",
+        borderwidth=2,
+        borderpad=4,
+        bgcolor="#ff7f0e",
+        opacity=0.7
+))  
+
 # plot tekst en layout
 fig.update_layout(
     title={
@@ -92,33 +112,7 @@ fig.update_layout(
     ) 
 )
 
-#laatste update om: 
-fig.add_annotation(
-        x=2,
-        y=5,
-        xref="x",
-        yref="y",
-        text="max=5",
-        showarrow=True,
-        font=dict(
-            family="Courier New, monospace",
-            size=16,
-            color="#ffffff"
-            ),
-        align="center",
-        arrowhead=2,
-        arrowsize=1,
-        arrowwidth=2,
-        arrowcolor="#636363",
-        ax=20,
-        ay=-30,
-        bordercolor="#c7c7c7",
-        borderwidth=2,
-        borderpad=4,
-        bgcolor="#ff7f0e",
-        opacity=0.8
-        )
-))      
+
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
