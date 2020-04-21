b0ff418919b1b6eeeec45acb2354fcfe0da2b5b0
thierryzard <62876206+thierryzard@users.noreply.github.com>
2020-04-21 11:33:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,108 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-
+#import modules
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import io
+import requests
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
 
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
+#binnen halen URL data
+url_sex = 'https://raw.githubusercontent.com/J535D165/CoronaWatchNL/master/data/rivm_NL_covid19_sex.csv'
+source_sex = requests.get(url_sex).content
+df_sex = pd.read_csv(io.StringIO(source_sex.decode('utf-8')))
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
+
+#selecteerd laatste datum
+pd.to_datetime(df_sex['Datum'])
+recent_datum = df_sex['Datum'].max()
+recent_df= df_sex[df_sex['Datum']== recent_datum]
+laatste_update = pd.to_datetime(recent_datum)
+
+
+#split vrouwen/mannen
+man= recent_df[recent_df["Geslacht"]== 'Man']
+vrouw= recent_df[recent_df["Geslacht"]== 'Vrouw']
+
+#plot figuur
+fig = go.Figure(data=[
+    go.Bar(name='Man', x=man['Type'], y=man['Aantal'],marker_color='#5684FF'),
+    go.Bar(name='Vrouw', x=vrouw['Type'], y=vrouw['Aantal'], marker_color='#E3127B')
+])
+
+
+# plot tekst en layout
+fig.update_layout(
+    title={
+        'text': "Man/Vrouw verdeling positief getest voor corona",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    titlefont_size=30,
+    autosize=True,
     barmode='group',
-    title = mytitle
+    yaxis_title="Aantal Mensen positief voor corona",
+    plot_bgcolor='#f5f7fa',
+    #x-axis
+    xaxis=dict(
+        {'categoryorder':'array', 'categoryarray':['Ziekenhuisopname', 'Overleden','Totaal']},
+        tickfont_size=14
+        ),
+    #y-axis
+    yaxis=dict(
+        title="Aantal mensen positief getest",
+        titlefont_size=18,
+        tickfont_size=14
+    ),
+    #balken
+    bargap=0.6,
+    bargroupgap=0.1,
+    #legenda
+    legend=dict(
+        x=0.1,
+        y=1.0,
+        bgcolor='white',
+        bordercolor ='black',
+        borderwidth = 1,
+        font=dict(
+            size=12,
+            color="#000000"
+            )
+    ) 
 )
 
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
+#laatste update om: 
+fig.add_annotation(
+    dict(
+            x=0.99,
+            y=1,
+            showarrow=False,
+            text="geüpdatet op  {:%d/%m/%Y}".format(laatste_update),
+            xref="paper",
+            yref="paper",
+        font=dict(
+            size=10,
+            color="#FFFFFF"
+            ),
+        align="center",  
+        bordercolor="#c7c7c7",
+        borderwidth=2,
+        borderpad=4,
+        bgcolor="#ff7f0e",
+        opacity=0.7
+))      
+
+#aanpassen aan grote
+fig.update_yaxes(automargin=True)
+fig.update_xaxes(automargin=True)
+
+
+
+tabtitle='Corona'
 
 
 ########### Initiate the app
@@ -53,18 +113,12 @@ app.title=tabtitle
 
 ########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(myheading),
     dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
+        id='Corona',
+        figure=fig
+    )
     ]
 )
 
 if __name__ == '__main__':
     app.run_server()
-
-
