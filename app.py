35c78ff733839efe4bf18a0e0b1b9603c6f9fcdc
thierryzard <62876206+thierryzard@users.noreply.github.com>
2020-04-29 12:37:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 
 
 
-
-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-
-
 import plotly.graph_objects as go
 fig = go.Figure(
     data=[go.Bar(y=[2, 1, 3])],
     layout_title_text="A Figure Displayed with fig.show()"
 )
 
-app = dash.Dash()
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+########### Initiate the app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+
+
 app.layout = html.Div([
     dcc.Graph(figure=fig)
 ])
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+
+
